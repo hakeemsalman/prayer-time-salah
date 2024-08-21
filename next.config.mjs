b15f106c4030f79cc4a9c2d735ef723db3a82802
Salman S H <shsalman114@gmail.com>
2024-08-21 17:40:53 +0530

    modified next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',  // Apply to all routes
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
